d92f678528fe157b997b008832ccfa48af51b364
SHREYANSH <shreyanshjain2674@gmail.com>
2021-08-18 02:18:35 +0530

    INitial

--- app/src/main/java/com/codewithshreya/flobizassignment/MainActivity.java
+++ app/src/main/java/com/codewithshreya/flobizassignment/MainActivity.java
@@ -105,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
         RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
         requestQueue.add(stringRequest);
 
